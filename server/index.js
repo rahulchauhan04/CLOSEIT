8e710f9666514c9a41e86bcd35054a77ed762daa
rahulchauhan04 <rahulg0004@gmail.com>
2024-12-04 09:32:07 +0530

    user model

--- server/index.js
+++ server/index.js
@@ -1,10 +1,36 @@
 import express from 'express';
 import cors from 'cors';
 import dotenv from 'dotenv';
+import cookieParser from 'cookie-parser';
+import morgan from 'morgan';
+import helmet from 'helmet';
+import connectDB from './config/connectDB.js';
 
 const app = express();
 
 app.use(cors({
      credentials: true,
-     origin: 'process.env.FRONTEND_URL'
+     origin: process.env.FRONTEND_URL
 }))
+
+app.use(express.json());
+app.use(cookieParser());
+app.use(morgan());
+app.use(helmet({
+     crossOriginResourcePolicy: false
+}))
+
+const PORT = 8080 || process.env.PORT
+
+app.get('/', (request, response) => {
+     response.json({
+          message: "Server is Running " + PORT
+     })
+})
+
+connectDB().then(() => {
+     app.listen(PORT, () => {
+          console.log("Server is running on port", PORT)
+     })
+});
+
