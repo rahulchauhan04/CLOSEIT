856c7381380b5e106821a025407bfce32a2407a4
rahulchauhan04 <rahulg0004@gmail.com>
2025-03-02 17:47:37 +0530

    subcategory-added

--- server/config/connectDB.js
+++ server/config/connectDB.js
@@ -2,19 +2,18 @@ import mongoose from 'mongoose';
 import dotenv from 'dotenv';
 dotenv.config();
 
-if(!process.env.MONGODB_URI){
-     throw new Error("MONGO_URL is not defined in .env file")
+if (!process.env.MONGODB_URI) {
+    throw new Error("MONGO_URL is not defined in .env file");
 }
 
 async function connectDB() {
-     try {
-          await mongoose.connect(process.env.MONGODB_URI)
-          console.log("DB Connected");
-          
-     } catch (error) {
-          console.log("Mongodb connection error", error);
-          process.exit(1);
-     }
+    try {
+        await mongoose.connect(process.env.MONGODB_URI);
+        console.log("DB Connected");
+    } catch (error) {
+        console.log("Mongodb connection error", error);
+        process.exit(1);
+    }
 }
 
 export default connectDB;
--- server/index.js
+++ server/index.js
@@ -8,6 +8,7 @@ import connectDB from './config/connectDB.js';
 import userRouter from './route/user.route.js';
 import categoryRouter from './route/category.route.js';
 import uploadRouter from './route/upload.router.js';
+import subCategoryRouter from './route/subCategory.route.js';
 
 const app = express();
 
@@ -34,6 +35,7 @@ app.get('/', (request, response) => {
 app.use('/api/user', userRouter)
 app.use('/api/category', categoryRouter)
 app.use('/api/file', uploadRouter)
+app.use('/api/subcategory', subCategoryRouter)
 
 connectDB().then(() => {
      app.listen(PORT, () => {
